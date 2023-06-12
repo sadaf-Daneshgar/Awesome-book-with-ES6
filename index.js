b3ad7a725ec93c1255b643050b08b836fb73ffed
“sadaf <sadaf.batekzai54321@gmial.com>
2023-06-12 17:41:30 +0430

    Add new modules

---
+++ index.js
@@ -0,0 +1,7 @@
+import Book from "./modules/addbook.js";
+import * as Navigation from './modules/navigation.js';
+
+const bookList = new Book('#book-form', 'bookCollection', '.booklist-table');
+Navigation.toggleElement('target');
+Navigation.toggleNav('targetNav');
+Navigation.attachEventListeners();
