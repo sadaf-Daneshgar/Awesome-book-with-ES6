957e0945b7ab04467d374a398fc8efb61918278b
“sadaf <sadaf.batekzai54321@gmial.com>
2023-06-13 11:48:27 +0430

    fix linter error

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import Book from './modules/addbook.js';
 import * as Navigation from './modules/navigation.js';
 import setDateTime from './modules/data&time.js';
 
-setDateTime();
 // eslint-disable-next-line no-unused-vars
 const bookList = new Book('#book-form', 'bookCollection', '.booklist-table');
 Navigation.toggleElement('target');
 Navigation.toggleNav('targetNav');
 Navigation.attachEventListeners();
+setDateTime();
